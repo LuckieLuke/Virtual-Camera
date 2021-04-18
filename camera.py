0aa7c4ece14e69d33c8ff09d1e79dd1cf5ef5b6d
LuckieLuke <luk19984@gmail.com>
2021-04-18 12:37:34 +0200

    eliminating hidden surfaces vol. 1

--- camera.py
+++ camera.py
@@ -1,6 +1,7 @@
 from matrices import *
-from utils import *
+import utils
 from tkinter import *
+from functools import cmp_to_key
 
 
 class Camera():
@@ -13,65 +14,79 @@ class Camera():
         self.my_canvas = Canvas(self.root, width=900, height=700, bg='white')
         self.my_canvas.pack(pady=70)
 
-        w = Label(self.root, text='''Keys: 
+        w = Label(self.root, text='''Keys:
         WSADCF - walking around (up, down, left, right, front, back)
-        QERTGH - rotating
+        QERTGH - rotating, PL - zoom
         ''')
         w.place(x=300, y=0)
 
-        self.lines = []
-        self.lines_coords = []
+        self.triangles = []
+        self.triangles_coords = []
         self.root.bind('<Key>', self.key_pressed)
 
         with open('coords.txt', 'r') as file:
-            for line in file.readlines():
+            for index, line in enumerate(file.readlines()):
                 if line[0] != '#':
                     coords = [int(x) for x in line.split()]
-                    start = np.array([[*coords[:3], 1]]).T
-                    end = np.array([[*coords[3:], 1]]).T
+                    a = np.array([[*coords[:3], 1]]).T
+                    b = np.array([[*coords[3:6], 1]]).T
+                    c = np.array([[*coords[6:], 1]]).T
 
-                    self.lines_coords.append((start, end))
-                    if (start[2][0] == -10 and end[2][0] == -10) or (start[2][0] == -210 and end[2][0] == -210):
-                        color = "red"
-                    elif (start[2][0] == -110 and end[2][0] == -110) or (start[2][0] == -310 and end[2][0] == -310):
-                        color = "green"
-                    else:
-                        color = "blue"
+                    self.triangles_coords.append((a, b, c, index))
 
-                    self.lines.append(
-                        self.my_canvas.create_line(start[0][0], start[1][0], end[0][0], end[1][0], fill=color, width=2))
+                    self.triangles.append(
+                        self.my_canvas.create_polygon(
+                            a[0][0], a[1][0],
+                            b[0][0], b[1][0],
+                            c[0][0], c[1][0],
+                            fill='blue',
+                            outline='red'
+                        )
+                    )
         self.render()
         self.root.mainloop()
 
     def key_pressed(self, event):
         if event.char == 'd':
-            move(self.lines_coords, 'left')
+            utils.move(self.triangles_coords, 'left')
+            # utils.move_cam('right')
         elif event.char == 'a':
-            move(self.lines_coords, 'right')
+            utils.move(self.triangles_coords, 'right')
+            # utils.move_cam('left')
         elif event.char == 's':
-            move(self.lines_coords, 'up')
+            utils.move(self.triangles_coords, 'up')
+            # utils.move_cam('down')
         elif event.char == 'w':
-            move(self.lines_coords, 'down')
+            utils.move(self.triangles_coords, 'down')
+            # utils.move_cam('up')
         elif event.char == 'c':
-            move(self.lines_coords, 'front')
+            utils.move(self.triangles_coords, 'front')
+            # utils.move_cam('back')
         elif event.char == 'f':
-            move(self.lines_coords, 'back')
+            utils.move(self.triangles_coords, 'back')
+            # utils.move_cam('front')
         elif event.char == 'q':
-            rotate(self.lines_coords, -0.01, 'z')
+            utils.rotate(self.triangles_coords, -0.01, 'z')
+            #utils.rotate_cam(0.01, 'z')
         elif event.char == 'e':
-            rotate(self.lines_coords, 0.01, 'z')
+            utils.rotate(self.triangles_coords, 0.01, 'z')
+            #utils.rotate_cam(-0.01, 'z')
         elif event.char == 'r':
-            rotate(self.lines_coords, -0.01, 'x')
+            utils.rotate(self.triangles_coords, -0.01, 'x')
+            #utils.rotate_cam(0.01, 'x')
         elif event.char == 't':
-            rotate(self.lines_coords, 0.01, 'x')
+            utils.rotate(self.triangles_coords, 0.01, 'x')
+            #utils.rotate_cam(-0.01, 'x')
         elif event.char == 'h':
-            rotate(self.lines_coords, -0.01, 'y')
+            utils.rotate(self.triangles_coords, -0.01, 'y')
+            #utils.rotate_cam(0.01, 'y')
         elif event.char == 'g':
-            rotate(self.lines_coords, 0.01, 'y')
+            utils.rotate(self.triangles_coords, 0.01, 'y')
+            #utils.rotate_cam(-0.01, 'y')
         elif event.char == 'p':
-            zoom(self.lines_coords, 2)
+            utils.zoom(self.triangles_coords, 2)
         elif event.char == 'l':
-            zoom(self.lines_coords, 0.5)
+            utils.zoom(self.triangles_coords, 0.5)
         self.render()
 
     def render(self):
@@ -89,24 +104,90 @@ class Camera():
             orth_view['n'],
             orth_view['f']
         )
-        Mcam = np.array([
-            [1, 0, 0, 0],
-            [0, 1, 0, 0],
-            [0, 0, 1, 0],
-            [0, 0, 0, 1]
-        ])
 
-        M = P.dot(Mcam)
-        M = Morth.dot(M)
+        M = Morth.dot(P)
         M = MVP.dot(M)
+        # screen_triangles = []
 
-        for index, coord in enumerate(self.lines_coords):
+        # for index, coord in enumerate(self.triangles_coords):
+        #     screen_coords_a = M.dot(coord[0])
+        #     screen_coords_b = M.dot(coord[1])
+        #     screen_coords_c = M.dot(coord[2])
+
+        #     screen_triangles.append(
+        #         (screen_coords_a, screen_coords_b, screen_coords_c, self.triangles[index]))
+
+        self.triangles_coords = sorted(
+            self.triangles_coords, key=cmp_to_key(is_closer))
+        # for triangle in self.triangles_coords:
+        #     print(triangle[3])
+        # print(self.triangles_coords)
+
+        # print(screen_triangles)
+        # print(self.triangles)
+
+        self.my_canvas.delete("all")
+
+        for index, coord in enumerate(self.triangles_coords):
             screen_coords_a = M.dot(coord[0])
             screen_coords_b = M.dot(coord[1])
+            screen_coords_c = M.dot(coord[2])
+            # print(screen_triangle[3])
+            # self.my_canvas.coords(self.triangles[screen_triangle[3]-1], [
+            #     int(screen_triangle[0][0] / screen_triangle[0][3]),
+            #     int(screen_triangle[0][1] / screen_triangle[0][3]),
+            #     int(screen_triangle[1][0] / screen_triangle[1][3]),
+            #     int(screen_triangle[1][1] / screen_triangle[1][3]),
+            #     int(screen_triangle[2][0] / screen_triangle[2][3]),
+            #     int(screen_triangle[2][1] / screen_triangle[2][3])
+            # ])
 
-            self.my_canvas.coords(self.lines[index], [
+            self.my_canvas.create_polygon(
                 int(screen_coords_a[0] / screen_coords_a[3]),
                 int(screen_coords_a[1] / screen_coords_a[3]),
                 int(screen_coords_b[0] / screen_coords_b[3]),
-                int(screen_coords_b[1] / screen_coords_b[3])
-            ])
+                int(screen_coords_b[1] / screen_coords_b[3]),
+                int(screen_coords_c[0] / screen_coords_c[3]),
+                int(screen_coords_c[1] / screen_coords_c[3]),
+                fill='blue',
+                outline='red'
+            )
+
+
+def is_closer(plane, check, *zapychadelko):
+    p1 = np.array(plane[0])
+    p2 = np.array(plane[1])
+    p3 = np.array(plane[2])
+
+    cp1 = np.array(check[0])
+    cp2 = np.array(check[1])
+    cp3 = np.array(check[2])
+
+    v1 = p3 - p1
+    v2 = p2 - p1
+
+    cp = np.cross(np.concatenate(v1[:3]), np.concatenate(v2[:3]))
+    a, b, c = cp
+
+    d = np.dot(cp, p3[:3])
+
+    cam_dir = 1 if a * \
+        utils.camera_coords[0][3] + b*utils.camera_coords[1][3] + c*utils.camera_coords[2][3] - d >= 0 else -1
+
+    tr_dir1 = a*cp1[0] + b*cp1[1] + c*cp1[2] - d
+    tr_dir2 = a*cp2[0] + b*cp2[1] + c*cp2[2] - d
+    tr_dir3 = a*cp3[0] + b*cp3[1] + c*cp3[2] - d
+
+    results = [np.round(x, 6) for x in [tr_dir1, tr_dir2, tr_dir3]]
+
+    # if plane[3] == 10 and check[3] == 25:
+    #     print(f'{tr_dir1} {tr_dir2} {tr_dir3}')
+
+    if not any(results):
+        return 0
+
+    # print(results)
+    check = [x for x in results if x != 0]
+    if cam_dir * check[0] < 0:
+        return 1
+    return -1
--- utils.py
+++ utils.py
@@ -2,6 +2,14 @@ from tkinter import *
 import numpy as np
 from matrices import *
 from const import *
+import math
+
+camera_coords = np.array(
+    [[1, 0, 0, 0],
+     [0, 1, 0, 0],
+     [0, 0, 1, 0],
+     [0, 0, 0, 1]]
+)
 
 
 def move(coords, direction):
@@ -14,7 +22,30 @@ def move(coords, direction):
             [[coord[1][0][0]], [coord[1][1][0]], [coord[1][2][0]], [1]])
         new_cords2 = trans(*MOVE_DIRECTION[direction]).dot(new_cords2)
 
-        coords[index] = (new_cords1, new_cords2)
+        new_cords3 = np.array(
+            [[coord[2][0][0]], [coord[2][1][0]], [coord[2][2][0]], [1]])
+        new_cords3 = trans(*MOVE_DIRECTION[direction]).dot(new_cords3)
+
+        coords[index] = (new_cords1, new_cords2, new_cords3, coord[3])
+
+
+def move_cam(direction):
+    global camera_coords
+
+    column, coef = None, None
+    if direction in ['up', 'down']:
+        coef = 3 if direction == 'down' else -3
+        column = camera_coords[:, 1]
+    elif direction in ['left', 'right']:
+        coef = 3 if direction == 'right' else -3
+        column = camera_coords[:, 0]
+    else:
+        coef = 3 if direction == 'front' else -3
+        column = camera_coords[:, 2]
+
+    column = column * coef
+    for i in range(4):
+        camera_coords[i, 3] += column[i]
 
 
 def rotate(coords, angle, axis):
@@ -23,18 +54,52 @@ def rotate(coords, angle, axis):
             [[coord[0][0][0]], [coord[0][1][0]], [coord[0][2][0]], [1]])
         new_cords2 = np.array(
             [[coord[1][0][0]], [coord[1][1][0]], [coord[1][2][0]], [1]])
+        new_cords3 = np.array(
+            [[coord[2][0][0]], [coord[2][1][0]], [coord[2][2][0]], [1]])
 
         if axis == 'x':
             new_cords1 = rotateX(angle).dot(new_cords1)
             new_cords2 = rotateX(angle).dot(new_cords2)
+            new_cords3 = rotateX(angle).dot(new_cords3)
         elif axis == 'y':
             new_cords1 = rotateY(angle).dot(new_cords1)
             new_cords2 = rotateY(angle).dot(new_cords2)
+            new_cords3 = rotateY(angle).dot(new_cords3)
         elif axis == 'z':
             new_cords1 = rotateZ(angle).dot(new_cords1)
             new_cords2 = rotateZ(angle).dot(new_cords2)
+            new_cords3 = rotateZ(angle).dot(new_cords3)
 
-        coords[index] = (new_cords1, new_cords2)
+        coords[index] = (new_cords1, new_cords2, new_cords3, coord[3])
+
+
+def rotate_cam(angle, axis):
+    global camera_coords
+
+    cam_rot = np.array([
+        [*camera_coords[0, :3], 0],
+        [*camera_coords[1, :3], 0],
+        [*camera_coords[2, :3], 0],
+        [0, 0, 0, 0],
+    ])
+
+    if axis == 'x':
+        cam_rot = rotateX(angle).dot(cam_rot)
+    elif axis == 'y':
+        cam_rot = rotateY(angle).dot(cam_rot)
+    elif axis == 'z':
+        cam_rot = rotateZ(angle).dot(cam_rot)
+
+    x, y, z = camera_coords[0, 3], camera_coords[1, 3], camera_coords[2, 3]
+    camera_coords = np.array([
+        [*cam_rot[0, :3], x],
+        [*cam_rot[1, :3], y],
+        [*cam_rot[2, :3], z],
+        [0, 0, 0, 1],
+    ])
+
+    angle = math.asin(camera_coords[2][0]) * 180 / math.pi
+    print(angle)
 
 
 def zoom(coords, coef):
@@ -43,8 +108,11 @@ def zoom(coords, coef):
             [[coord[0][0][0]], [coord[0][1][0]], [coord[0][2][0]], [1]])
         new_cords2 = np.array(
             [[coord[1][0][0]], [coord[1][1][0]], [coord[1][2][0]], [1]])
+        new_cords3 = np.array(
+            [[coord[2][0][0]], [coord[2][1][0]], [coord[2][2][0]], [1]])
 
         new_cords1 = mzoom(coef).dot(new_cords1)
         new_cords2 = mzoom(coef).dot(new_cords2)
+        new_cords3 = mzoom(coef).dot(new_cords3)
 
-        coords[index] = (new_cords1, new_cords2)
+        coords[index] = (new_cords1, new_cords2, new_cords3, coord[3])
